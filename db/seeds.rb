80bb67b400ed292666eaabbf44b437e30265af0e
Teapane <tyler.stephen.long@gmail.com>
2020-12-03 14:56:48 -0700

    Adds a seeder for random usernames

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def generate_random_usernames(number)
+  # use the whole alphabet
+  charset = Array('A'..'Z') + Array('a'..'z')
+  Array.new(number) {
+    charset.sample
+  }.join
+end
+
+def create_users
+  arr = []
+  1000.times do
+    arr << generate_random_usernames(15)
+  end
+  arr.each do |x|
+    puts "Creating user #{x}"
+    User.create(username: x)
+    puts "Username #{x} created!"
+  end
+end
+
+
+puts "Creating Users"
+create_users(arr)
+puts "User creation complete"
