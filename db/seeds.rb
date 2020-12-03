79c077be39dea9ee1d97e96e1a1af7ab17d2eeb8
Teapane <tyler.stephen.long@gmail.com>
2020-12-03 15:10:30 -0700

    Seeds with posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-def generate_random_usernames(number)
+def generate_random(number)
   # use the whole alphabet
   charset = Array('A'..'Z') + Array('a'..'z')
   Array.new(number) {
@@ -9,7 +9,7 @@ end
 def create_users
   arr = []
   1000.times do
-    arr << generate_random_usernames(15)
+    arr << generate_random(15)
   end
   arr.each do |x|
     puts "Creating user #{x}"
@@ -18,7 +18,25 @@ def create_users
   end
 end
 
+# [4] pry(main)> Post
+# => Post(id: integer, user_id: integer, title: string, created_at: datetime, updated_at: datetime)
+#
+def generate_posts
+  arr = []
+  1000.times do
+    arr << generate_random(15)
+  end
+  arr.each do |x|
+    puts "Creating Post #{x}"
+    user_id = User.order(Arel.sql('RANDOM()')).first.id
+    Post.create!(user_id: user_id, title: x)
+  end
+end
 
 puts "Creating Users"
 create_users
 puts "User creation complete"
+
+puts "Creating Posts"
+generate_posts
+puts "Posts creation complete"
